ccf0afc90665d08bca96a224084e1b60df50fdb1
Raymond Willett <rwillet2@emich.edu>
2019-03-30 18:03:37 -0400

    Group tests based on what aspect they are testing

--- cart/cart.test.js
+++ cart/cart.test.js
@@ -10,10 +10,14 @@ afterEach(() => {
     cart = null;
 });
 
-test('That cart exists', () => {
-    expect(cart).toBeDefined();
-});
+describe('The cart module', () => {
+    test('That cart exists', () => {
+        expect(cart).toBeDefined();
+    });
+})
 
-test('That cart has a a productLineItems property', () => {
-    expect(cart.productLineItems).toBeDefined();
+describe('The cart\'s product line items', () => {
+    test('That cart has a a productLineItems property', () => {
+        expect(cart.productLineItems).toBeDefined();
+    });
 });
