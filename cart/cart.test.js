d41d66e4f38a333dca228f2cecbd014b19e4b11b
Raymond Willett <rwillet2@emich.edu>
2019-03-30 18:01:07 -0400

    Add productLineItems property to the cart

--- cart/cart.test.js
+++ cart/cart.test.js
@@ -13,3 +13,7 @@ afterEach(() => {
 test('That cart exists', () => {
     expect(cart).toBeDefined();
 });
+
+test('That cart has a a productLineItems property', () => {
+    expect(cart.productLineItems).toBeDefined();
+});
--- cart/index.js
+++ cart/index.js
@@ -1,6 +1,6 @@
 class Cart {
     constructor () {
-        return;
+        this.productLineItems = {};
     }
 }
 
