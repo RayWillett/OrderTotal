9f3a590b15eb883aafd8c706156992c77779b7e0
Raymond Willett <rwillet2@emich.edu>
2019-03-30 18:34:58 -0400

    Add function header comments to the Cart class

--- cart/index.js
+++ cart/index.js
@@ -1,7 +1,17 @@
 class Cart {
+    /**
+     * Represents the  cart object.
+     * @constructor
+     */
     constructor () {
         this.productLineItems = {};
     }
+
+    /**
+     * Allows an item to be added to the cart at a given quantity.
+     * @param {string} productID - The ID of the product being added to the cart.
+     * @param {number} quantity = The quantity of the product to be added to the cart.
+     */
     addItem (productID, quantity) {
         // If the productID isn't already in the cart, create a new line item wrapper for it.
         if (!this.productLineItems.hasOwnProperty(productID)) {
@@ -12,6 +22,8 @@ class Cart {
         // Increase line item quantity
         this.productLineItems[productID].quantity += quantity;
     }
+
+
 }
 
 module.exports = Cart;
