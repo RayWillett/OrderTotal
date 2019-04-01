09d154989e84143d90f4d2e904d8ea63461817ab
Raymond Willett <rwillet2@emich.edu>
2019-03-31 21:07:36 -0400

    Add the getApplicablePromotions method to the PromotionManager class

--- promotionManager/index.js
+++ promotionManager/index.js
@@ -8,6 +8,16 @@ class PromotionManager {
     constructor (availablePromotions) {
         this.availablePromotions = availablePromotions;
     }
+
+    /**
+     * Returns a list of promotions which can/will be applied to the cart.
+     *
+     *@param listOfProducts {array} - the list of products which may have an available promotion in the cart.
+     *@returns {array} - The list of promotions which are applicable to the cart in its current state.
+     */
+     getApplicablePromotions (listOfProducts) {
+        return [];
+     }
 }
 
 module.exports = PromotionManager;
