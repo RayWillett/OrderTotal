97f0bbd58c2e367b1c7af0bb4226424c83ec4832
Raymond Willett <rwillet2@emich.edu>
2019-04-01 18:46:00 -0400

    Remove PromotionManager methods. Change the API.

--- promotionManager/index.js
+++ promotionManager/index.js
@@ -8,26 +8,6 @@ class PromotionManager {
     constructor (availablePromotions) {
         this.availablePromotions = availablePromotions;
     }
-
-    /**
-     * Returns a list of promotions which can/will be applied to the cart.
-     *
-     *@param listOfProducts {array} - the list of products which may have an available promotion in the cart.
-     *@returns {array} - The list of promotions which are applicable to the cart in its current state.
-     */
-     getApplicablePromotions (listOfProducts) {
-        return [];
-     }
-
-     /**
-      * Returns an available promotion for the given product ID or and empty array if none are found.
-      *
-      * @param productID {string} - the product ID to search promotions for.
-      * @returns {array} - the product's promotions or an empty array.
-      */
-     getProductPromotion (productID) {
-        return this.availablePromotions.filter( promotion => promotion.buy.ID == productID );
-     }
 }
 
 module.exports = PromotionManager;
--- promotionManager/promotionManager.test.js
+++ promotionManager/promotionManager.test.js
@@ -21,39 +21,4 @@ describe('The PromotionManager class', () => {
         expect(promotionManager.availablePromotions).toBeDefined();
         expect(promotionManager.availablePromotions).toBeInstanceOf(Array);
     });
-
-    test('That getApplicablePromotions exists', () => {
-        expect(promotionManager.getApplicablePromotions).toBeDefined();
-        expect(typeof promotionManager.getApplicablePromotions).toBe('function');
-    });
-
-    test('That getProductPromotion exists', () => {
-        expect(promotionManager.getProductPromotion).toBeDefined();
-        expect(typeof promotionManager.getProductPromotion).toBe('function');
-    });
-});
-
-describe('The getProductPromotion method', () => {
-    test('That an invalid product ID results in an undefined promotion', () => {
-        const fakeProductID = 'fake product';
-        const productPromotion = promotionManager.getProductPromotion(fakeProductID);
-        expect(productPromotion).toEqual([]);
-    });
-
-    test('That a valid product ID results in a promotion', () => {
-        const existingProductID = promoData[0].buy.ID;
-        const productPromotion = promotionManager.getProductPromotion(existingProductID);
-        expect(productPromotion).toBeDefined();
-        expect(productPromotion).toEqual([promoData[0]]);
-    });
-
-    test('That a valid product ID can result in multiple promotions', () => {
-        const existingProductID = promoData[1].buy.ID;
-        const productPromotion = promotionManager.getProductPromotion(existingProductID);
-        expect(productPromotion).toBeDefined();
-        expect(productPromotion).toEqual([
-            promoData[1],
-            promoData[2]
-        ]);
-    });
 });
