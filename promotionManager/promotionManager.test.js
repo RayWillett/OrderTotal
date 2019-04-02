23570a4dc9be5e06d07bc300a7c32114f5184058
Raymond Willett <rwillet2@emich.edu>
2019-04-02 19:44:51 -0400

    Refactor tests to use hard coded data rather than duplicate the functionality

--- promotionManager/promotionManager.test.js
+++ promotionManager/promotionManager.test.js
@@ -120,16 +120,11 @@ describe('The limit property of a promotion', () => {
     });
 
     test('That it returns the correct discount to the products\'s price if the promotion is a "3 for $5 limit X" type', () => {
-        const productPrice = 8.00,
-            promotion = promoData.limitedPromotions[2],
-            quantitiesToTest = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+        const promotion = promoData.limitedPromotions[2],
+            { data, productPrice } = promotion.test;
 
-        quantitiesToTest.forEach(quantity => {
-            const markdownAmount = promotionManager.getDiscountAmount(promotion, quantity, productPrice),
-                appliedTo = Math.min(quantity, promotion.limit),
-                bundledProducts = Math.floor(appliedTo / promotion.quantityNeeded),
-                remainingProducts = (appliedTo % promotion.quantityNeeded) + (quantity - appliedTo),
-                expectedDiscountAmount = (productPrice * quantity) - ((promotion.newPrice * bundledProducts) + (remainingProducts * productPrice));
+        data.forEach(({ quantity, expectedDiscountAmount }) => {
+            const markdownAmount = promotionManager.getDiscountAmount(promotion, quantity, productPrice)
 
             expect(markdownAmount).toBe(expectedDiscountAmount);
         });
