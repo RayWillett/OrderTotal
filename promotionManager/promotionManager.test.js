aa16a31d696574a9fcd8147359b9c9096abaefa0
Raymond Willett <rwillet2@emich.edu>
2019-04-02 19:51:59 -0400

    Add test for roundToNearestCent method to confirm it works

--- promotionManager/promotionManager.test.js
+++ promotionManager/promotionManager.test.js
@@ -137,7 +137,45 @@ describe('The roundToNearestCent method', () => {
 
         dollarAmounts.forEach(dollarAmount => {
             const roundedAmount = promotionManager.roundToNearestCent(dollarAmount);
-            expect(dollarAmount).toBe(roundedAmount)
+            expect(roundedAmount).toBe(dollarAmount)
+        });
+    });
+
+    test('that a partial number of cents is changed', () => {
+        const dollarAmounts = [
+            {
+                preRoundedAmount: 0.255,
+                postRoundAmount: 0.26
+            },
+            {
+                preRoundedAmount: 0.866666,
+                postRoundAmount: 0.87
+            },
+            {
+                preRoundedAmount: 0.00001,
+                postRoundAmount: 0
+            },
+            {
+                preRoundedAmount:  9.553,
+                postRoundAmount: 9.55
+            },
+            {
+                preRoundedAmount: 111111112222.3338838484848499999,
+                postRoundAmount: 111111112222.33
+            },
+            {
+                preRoundedAmount: 9.999,
+                postRoundAmount: 10.00
+            },
+            {
+                preRoundedAmount: (0.1 * 0.2), // floating point error
+                postRoundAmount: 0.02
+            }
+        ];
+
+        dollarAmounts.forEach(({ preRoundedAmount, postRoundAmount}) => {
+            const roundedAmount = promotionManager.roundToNearestCent(preRoundedAmount);
+            expect(roundedAmount).toBe(postRoundAmount)
         });
     });
 });
