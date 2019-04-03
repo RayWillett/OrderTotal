e77668262e25db7d2ca95a88e065c3d9a8394da4
Raymond Willett <rwillet2@emich.edu>
2019-04-03 18:39:48 -0400

    Add test for the add item flow

--- CartFactory/tests/integration.test.js
+++ CartFactory/tests/integration.test.js
@@ -14,3 +14,23 @@ describe('Creating a Cart instance', () => {
         expect(cart).toBeDefined();
     });
 });
+
+
+describe('Adding products to the cart', () => {
+    test('That the cart has the correct number of unique items in it', () => {
+        let productID = products[1].ID;
+
+        expect(Object.keys(cart.productLineItems).length).toBe(0);
+        
+        cart.addItem(productID, 1);
+        expect(Object.keys(cart.productLineItems).length).toBe(1);
+        
+        cart.addItem(productID, 1);
+        cart.addItem(productID, 2);
+        expect(Object.keys(cart.productLineItems).length).toBe(1);
+
+        productID = products[3].ID;
+        cart.addItem(productID, 1);
+        expect(Object.keys(cart.productLineItems).length).toBe(2);
+    });
+});
